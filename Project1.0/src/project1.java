ae80fb526fe24b6dfc8b895f1e97d2274a8256c4
asdf77764 <949177764@qq.com>
2016-10-17 17:07:35 +0800

    commit1

--- Project1.0/src/project1.java
+++ Project1.0/src/project1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class project1 {
-	public static void main(String[] args)
+	public static void main(Stri1ng[] args)
 	{
 		Scanner scanner = new Scanner(System.in);
 		String str=null;
